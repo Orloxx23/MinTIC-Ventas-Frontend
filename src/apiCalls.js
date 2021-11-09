9be98bed6f5dbce92bbfab6347b05a8a35f42683
Orlando Mina <madorlix@gmail.com>
2021-11-09 17:01:04 -0500

    Update apiCalls.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const loginCall = async (userCredentials, dispatch) => {
   dispatch({ type: 'LOGIN_START' });
   try {
-    const res = await axios.post('https://mintic-ventas-backend.herokuapp.com/api/auth/login', userCredentials);
+    const res = await axios.post('auth/login', userCredentials);
     dispatch({ type: 'LOGIN_SUCCESS', payload: res.data, error: false });
   } catch (err) {
     dispatch({ type: 'LOGIN_FAILURE', payload: err, error: true });
