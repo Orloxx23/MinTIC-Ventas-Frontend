22da4be70c1d5c51704aded8505432f7143b4c42
Orlando Mina <madorlix@gmail.com>
2021-11-09 23:03:25 -0500

    Update Products.js

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -68,7 +68,7 @@ function getComparator(order, orderBy) {
 }
 
 function applySortFilter(array, comparator, query) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
+  const stabilizedThis = array?.map((el, index) => [el, index]);
   stabilizedThis.sort((a, b) => {
     const order = comparator(a[0], b[0]);
     if (order !== 0) return order;
